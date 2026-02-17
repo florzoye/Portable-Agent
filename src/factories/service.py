106b2ff14c30933201ae9c9cfea0839c774e1daf
florzoye <florzoye.official@gmail.com>
2026-02-17 18:13:52 +0700

    add factory for FastAPI Depends

---
+++ src/factories/service.py
@@ -0,0 +1,32 @@
+from data.init_configs import get_config
+from src.enum.db import DatabaseType
+from src.services.calendar.google_calendar import GoogleCalendarService
+
+class ServiceFactory:
+    @staticmethod
+    async def create_google_calendar_service(session):
+
+        cfg = get_config()
+
+        if cfg.DB_CONFIG.DB_TYPE.lower() == DatabaseType.SQLITE.value:
+            from db.sqlite.user_crud import UsersSQLite
+            from db.sqlite.google_crud import TokensSQLite
+
+            users_repo = UsersSQLite(session)
+            tokens_repo = TokensSQLite(session)
+
+        else:
+            from db.sqlalchemy.user_crud import UsersORM
+            from db.sqlalchemy.google_crud import GoogleTokensORM
+
+            users_repo = UsersORM(session)
+            tokens_repo = GoogleTokensORM(session)
+
+        service = GoogleCalendarService(
+            users_repo=users_repo,
+            tokens_repo=tokens_repo,
+            client_id=cfg.GOOGLE_CONFIG.GOOGLE_CLIENT_ID,
+            client_secret=cfg.GOOGLE_CONFIG.GOOGLE_CLIENT_SECRET,
+        )
+
+        return service, session
