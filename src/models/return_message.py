81a24b5e5f435d1edceb072dcd9f3f9f9df1e592
florzoye <florzoye.official@gmail.com>
2026-02-17 22:27:23 +0700

    add base return message for responses

---
+++ src/models/return_message.py
@@ -0,0 +1,12 @@
+from data.init_configs import get_config
+
+async def status(message: str) -> dict[str]:
+    return {'status': message}
+
+async def health_check_message(database) -> dict:
+    return {
+        "status": "healthy",
+        "database": database.db_type,
+        "database_initialized": database.is_initialized,
+        "config_initialized": get_config().is_initialized
+    }
