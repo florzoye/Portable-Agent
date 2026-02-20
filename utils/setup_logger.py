51e086f42a0d62c3f1deaa08f8e9fc65fd0fb699
florzoye <florzoye.official@gmail.com>
2026-02-20 19:09:25 +0700

    setup logger func

---
+++ utils/setup_logger.py
@@ -0,0 +1,12 @@
+import sys
+import logging
+
+def setup_logging():
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
+        datefmt="%H:%M:%S",
+        handlers=[
+            logging.StreamHandler(sys.stdout)  
+        ],
+    )
