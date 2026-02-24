1546fb2f6f95389fc8f43d1a921442f4472cecad
florzoye <florzoye.official@gmail.com>
2026-02-24 21:30:23 +0700

    MCP_CALENDAR_URL add new const

--- utils/const.py
+++ utils/const.py
@@ -1,3 +1,5 @@
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 GOOGLE_CALENDAR_REDIRECT_URI = 'http://localhost:8001/calendar/oauth/callback'
+
 GOOGLE_CALENDAR_URI = 'http://localhost:8001'
+MCP_CALENDAR_URL = "http://localhost:8002/sse"
