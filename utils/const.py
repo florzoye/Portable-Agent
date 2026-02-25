2e7ad14150a9ac3f901ce8a4567a8f0fa535ccb1
florzoye <florzoye.official@gmail.com>
2026-02-25 12:20:33 +0700

    hardcode new ports

--- utils/const.py
+++ utils/const.py
@@ -1,8 +1,10 @@
-from data import get_config
-ports_config = get_config().PORTS_CONFIG
-
 SCOPES = ['https://www.googleapis.com/auth/calendar']
-GOOGLE_CALENDAR_REDIRECT_URI = f'http://localhost:{ports_config.FASTAPI_CALENDAR_PORT}/calendar/oauth/callback'
 
-GOOGLE_CALENDAR_URI = f'http://localhost:{ports_config.FASTAPI_CALENDAR_PORT}'
-MCP_CALENDAR_URL = f"http://localhost:{ports_config.MCP_CALENDAR_PORT}/sse"
+FASTAPI_CALENDAR_PORT = 8001
+MCP_CALENDAR_PORT = 8002
+
+GOOGLE_CALENDAR_REDIRECT_URI = f'http://localhost:{FASTAPI_CALENDAR_PORT}/calendar/oauth/callback'
+
+GOOGLE_CALENDAR_URI = f'http://localhost:{FASTAPI_CALENDAR_PORT}'
+
+MCP_CALENDAR_URL = f'http://localhost:{MCP_CALENDAR_PORT}/sse'
