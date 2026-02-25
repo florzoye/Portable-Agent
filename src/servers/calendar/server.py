4eb6089cbc0442cd6684ae75ab0a2b13a354692b
florzoye <florzoye.official@gmail.com>
2026-02-25 12:20:15 +0700

    new const

--- src/servers/calendar/server.py
+++ src/servers/calendar/server.py
@@ -1,10 +1,9 @@
 import uvicorn
 from src.servers.base import create_app
 from src.servers.calendar.google_calendar_api import router
+from utils.const import FASTAPI_CALENDAR_PORT
 
 app = create_app(title="Google Calendar Service", routers=[router])
 
 if __name__ == "__main__":
-    from data import get_config
-    ports_config = get_config().PORTS_CONFIG
-    uvicorn.run(app, host="0.0.0.0", port=ports_config.FASTAPI_CALENDAR_PORT)
+    uvicorn.run(app, host="0.0.0.0", port=FASTAPI_CALENDAR_PORT)
