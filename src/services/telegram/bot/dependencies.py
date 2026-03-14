8f2f3aff2c735df2711480a5804ed688484fcf28
florzoye <florzoye.official@gmail.com>
2026-03-14 14:29:26 +0700

    intagreted new tools factory

--- src/services/telegram/bot/dependencies.py
+++ src/services/telegram/bot/dependencies.py
@@ -1,25 +1,26 @@
 from langgraph.graph.state import CompiledStateGraph
 
 from src.agents.llms.initializer import LLMInitializer
-from src.agents.tools.calendar import get_calendar_tools
-from src.agents.prompts.system import AgentSystemPrompt
 from src.factories.agents_factory import AgentsFactory
+from src.agents.prompts.system import AgentSystemPrompt
 from src.factories.checkpointer_factory import get_checkpointer
 
+from src.factories.tools_factory import get_tools
 
 async def get_agent(tg_id: int) -> CompiledStateGraph:
     """
-    Dependency: создаёт агента для конкретного пользователя.
+    Dependency: creates an agent for a specific user.
 
-    CompiledStateGraph stateless — состояние диалога персистируется
-    в AsyncPostgresSaver.
+    CompiledStateGraph stateless — the dialog state is persisted
+    in AsyncPostgresSaver.
     """
     checkpointer = await get_checkpointer()
+    tools = await get_tools()
 
     return await AgentsFactory(
         name="tg-assistant",
-        model=LLMInitializer.get_llms()[0],
-        tools=await get_calendar_tools(),
+        model=LLMInitializer.get_selected(),
+        tools=tools,
         system_prompt=AgentSystemPrompt(),
         checkpointer=checkpointer,
         tg_id=tg_id,
